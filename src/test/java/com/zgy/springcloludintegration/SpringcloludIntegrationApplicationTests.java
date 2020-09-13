ce005bc95bc70fe98f0a4789a3c513a75c58193d
zuiguangyin <2247359268@qq.com>
2020-09-13 13:22:27 +0800

    springcloud-init

---
+++ src/test/java/com/zgy/springcloludintegration/SpringcloludIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.springcloludintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloludIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
