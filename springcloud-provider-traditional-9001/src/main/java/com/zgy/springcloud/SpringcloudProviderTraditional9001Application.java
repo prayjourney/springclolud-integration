711cb1720cac3d1cf567424c66ec8838bd82d737
zuiguangyin <2247359268@qq.com>
2020-09-13 17:57:38 +0800

    🌈🌈添加了调用api的服务提供者

---
+++ springcloud-provider-traditional-9001/src/main/java/com/zgy/springcloud/SpringcloudProviderTraditional9001Application.java
@@ -0,0 +1,13 @@
+package com.zgy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudProviderTraditional9001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudProviderTraditional9001Application.class, args);
+    }
+
+}
---
+++ springcloud-provider-traditional-9001/src/test/java/com/zgy/springcloud/SpringcloudProviderTraditional9001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudProviderTraditional9001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
