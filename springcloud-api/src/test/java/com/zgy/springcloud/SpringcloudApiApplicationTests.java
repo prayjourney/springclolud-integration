2559a19a5eb4a291dea514af3675596860199d8d
zuiguangyin <2247359268@qq.com>
2020-09-13 17:35:16 +0800

    操作的基础api

--- springcloud-api/src/test/java/com/zgy/springcloud/SpringcloudApiApplicationTests.java
+++ springcloud-api/src/test/java/com/zgy/springcloud/SpringcloudApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.zgy.springcloludintegration;
+package com.zgy.springcloud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringcloludIntegrationApplicationTests {
+class SpringcloudApiApplicationTests {
 
     @Test
     void contextLoads() {
