966f8ca6ed5566693a04512a1722892c512a0b1d
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-09-14 11:39:46 +0800

    🐦🐦简化类名

--- springcloud-traditional-consumer-9011/src/main/java/com/zgy/springcloud/SpringcloudTraditionalConsumer9011.java
+++ springcloud-traditional-consumer-9011/src/main/java/com/zgy/springcloud/SpringcloudTraditionalConsumer9011.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringcloudConsumerTraditional9011Application {
+public class SpringcloudTraditionalConsumer9011 {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudConsumerTraditional9011Application.class, args);
+        SpringApplication.run(SpringcloudTraditionalConsumer9011.class, args);
     }
 
 }
--- springcloud-traditional-provider-9001/src/main/java/com/zgy/springcloud/SpringcloudTraditionalProvider9001.java
+++ springcloud-traditional-provider-9001/src/main/java/com/zgy/springcloud/SpringcloudTraditionalProvider9001.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringcloudProviderTraditional9001Application {
+public class SpringcloudTraditionalProvider9001 {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudProviderTraditional9001Application.class, args);
+        SpringApplication.run(SpringcloudTraditionalProvider9001.class, args);
     }
 
 }
