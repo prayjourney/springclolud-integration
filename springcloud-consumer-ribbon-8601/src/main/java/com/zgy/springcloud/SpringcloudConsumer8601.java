8d810af12d1b887029cb12801579493eec02becc
zuiguangyin <2247359268@qq.com>
2020-09-15 02:19:38 +0800

    👑👑👑实现了基本的客户端负载均衡

--- springcloud-consumer-ribbon-8601/src/main/java/com/zgy/springcloud/SpringcloudConsumer8601.java
+++ springcloud-consumer-ribbon-8601/src/main/java/com/zgy/springcloud/SpringcloudConsumer8601.java
@@ -2,8 +2,11 @@ package com.zgy.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableEurekaClient
 public class SpringcloudConsumer8601 {
 
     public static void main(String[] args) {
---
+++ springcloud-consumer-ribbon-8601/src/main/java/com/zgy/springcloud/controller/PoliceConsumer8601Controller.java
@@ -0,0 +1,35 @@
+package com.zgy.springcloud.controller;
+
+import com.zgy.springcloud.pojo.Police;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Author: renjiaxin
+ * @Despcription:
+ * @Date: Created in 2020/9/14 9:15
+ * @Modified by:
+ */
+@Slf4j
+@RestController
+@RequestMapping("consumer8601/police")
+public class PoliceConsumer8601Controller {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    // private static final String restURL = "http://127.0.0.1:8001";
+    // 上面是传统的方式，下面是通过注册中心的方式，就可以获取我们想要的信息了
+
+    // 使用了Ribbon之后, 这个地址应该是一个变量，通过服务名来访问, 下面两种都可以
+    // private static final String restURL = "http://springcloud-provider-8001";
+    private static final String restURL = "http://SPRINGCLOUD-PROVIDER-8001";
+    @GetMapping("get")
+    public Police get(Integer id) {
+        log.info("准备从远程获取police, id是：{}!", id);
+        return restTemplate.getForObject(restURL + "/police/get?id=" + id, Police.class);
+    }
+}
