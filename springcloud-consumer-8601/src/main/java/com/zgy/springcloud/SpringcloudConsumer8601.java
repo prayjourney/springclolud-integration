653bee03303477a0fc1b7a264c7caf87b13633a1
zuiguangyin <2247359268@qq.com>
2020-09-15 01:11:46 +0800

    🎁🎁🎁初始化这个消费者

---
+++ springcloud-consumer-8601/src/main/java/com/zgy/springcloud/SpringcloudConsumer8601.java
@@ -0,0 +1,13 @@
+package com.zgy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudConsumer8601 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConsumer8601.class, args);
+    }
+
+}
---
+++ springcloud-consumer-8601/src/test/java/com/zgy/springcloud/SpringcloudConsumer8601ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudConsumer8601ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
