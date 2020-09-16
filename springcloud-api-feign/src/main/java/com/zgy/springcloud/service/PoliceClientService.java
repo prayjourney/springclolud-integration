70c4372a69887cff20f9d87cd4bb9373f57d338a
zuiguangyin <2247359268@qq.com>
2020-09-17 02:00:13 +0800

    🎱🎱🎱feign服务的关键

--- springcloud-api-feign/src/main/java/com/zgy/springcloud/service/PoliceClientService.java
+++ springcloud-api-feign/src/main/java/com/zgy/springcloud/service/PoliceClientService.java
@@ -15,6 +15,9 @@ import java.util.List;
  * @Date 2020/9/16
  * @Description 该接口对应于应用id为SPRINGCLOUD-PROVIDER的微服务
  */
+/**
+ * 这个是一个关键，我们使用feign的服务，去调用provider服务提供者的服务，这时候，我们就去会从@FeignClient注解的服务之中，找相同声明的方法，然后实际调用
+ */
 // 该接口对应于应用id为SPRINGCLOUD-PROVIDER的微服务
 @Component // 👕👕👕feign的接口，添加到spring管理
 @FeignClient(value = "SPRINGCLOUD-PROVIDER")
