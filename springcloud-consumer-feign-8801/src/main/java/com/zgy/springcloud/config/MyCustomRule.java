5d5d4bd9dd1019da5cb3242d8ade3bd324310e17
zuiguangyin <2247359268@qq.com>
2020-09-17 00:54:50 +0800

    🎗👕👕使用feign的consumer

---
+++ springcloud-consumer-feign-8801/src/main/java/com/zgy/springcloud/config/MyCustomRule.java
@@ -0,0 +1,80 @@
+package com.zgy.springcloud.config;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AbstractLoadBalancerRule;
+import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.loadbalancer.Server;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+/**
+ * @Author renjiaxin
+ * @Date 2020/9/15
+ * @Description
+ */
+@Configuration
+public class MyCustomRule{
+}
+//// 暂时屏蔽自定义的Rule
+//@Configuration
+//public class MyCustomRule extends AbstractLoadBalancerRule {
+//    // https://blog.csdn.net/weixin_43774824/article/details/86624762
+//    // https://blog.csdn.net/xxc1605629895/article/details/89045566
+//    // https://www.cnblogs.com/ITPower/p/13295955.html
+//    // total = 0 // 当total==5以后，我们指针才能往下走，
+//    // index = 0 // 当前对外提供服务的服务器地址，
+//    // total需要重新置为零，但是已经达到过一个5次，我们的index = 1
+//
+//    private int total = 0;    // 总共被调用的次数，目前要求每台被调用5次
+//    private int currentIndex = 0; // 当前提供服务的机器号
+//
+//    public Server choose(ILoadBalancer lb, Object key) {
+//        if (lb == null) {
+//            return null;
+//        }
+//        Server server = null;
+//        while (server == null) {
+//            if (Thread.interrupted()) {
+//                return null;
+//            }
+//
+//            List<Server> upList = lb.getReachableServers();
+//            List<Server> allList = lb.getAllServers();
+//            int serverCount = allList.size();
+//            if (serverCount == 0) {
+//                return null;
+//            }
+//            if (total < 5) {
+//                server = upList.get(currentIndex);
+//                total++;
+//            } else {
+//                total = 0;
+//                currentIndex++;
+//                if (currentIndex >= upList.size()) {
+//                    currentIndex = 0;
+//                }
+//            }
+//            if (server == null) {
+//                Thread.yield();
+//                continue;
+//            }
+//            if (server.isAlive()) {
+//                return (server);
+//            }
+//            server = null;
+//            Thread.yield();
+//        }
+//        return server;
+//    }
+//
+//    @Override
+//    public void initWithNiwsConfig(IClientConfig iClientConfig) {
+//
+//    }
+//
+//    @Override
+//    public Server choose(Object o) {
+//        return choose(getLoadBalancer(), o);
+//    }
+//}
