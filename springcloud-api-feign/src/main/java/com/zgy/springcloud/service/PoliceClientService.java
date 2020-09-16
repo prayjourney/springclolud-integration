37973b518ae345cf57f54913526c19fcd6f1d75e
zuiguangyin <2247359268@qq.com>
2020-09-17 01:59:14 +0800

    🏀🏀🏀调整feign接收的参数

--- springcloud-api-feign/src/main/java/com/zgy/springcloud/service/PoliceClientService.java
+++ springcloud-api-feign/src/main/java/com/zgy/springcloud/service/PoliceClientService.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,20 +18,19 @@ import java.util.List;
 // 该接口对应于应用id为SPRINGCLOUD-PROVIDER的微服务
 @Component // 👕👕👕feign的接口，添加到spring管理
 @FeignClient(value = "SPRINGCLOUD-PROVIDER")
-public interface PoliceClientService
-{
-    @PostMapping(value = "police/add")
-    public int addPolice(Police police);
+public interface PoliceClientService {
+    @PostMapping(value = "/police/add")
+    public int addPolice(@RequestBody Police police);
 
-    @GetMapping(value = "police/del")
-    public int delPolice(Integer id);
+    @GetMapping(value = "/police/del")
+    public int delPolice(@RequestParam("id") Integer id);
 
-    @PostMapping(value = "police/update")
-    public int updatePolice(Police police);
+    @PostMapping(value = "/police/update")
+    public int updatePolice(@RequestBody Police police);
 
-    @GetMapping(value = "police/get")
-    public Police getPolice(Integer id);
+    @GetMapping(value = "/police/get")
+    public Police getPolice(@RequestParam("id") Integer id);
 
-    @GetMapping(value = "police/getall")
+    @GetMapping(value = "/police/getall")
     public List<Police> getAllPolice();
 }
