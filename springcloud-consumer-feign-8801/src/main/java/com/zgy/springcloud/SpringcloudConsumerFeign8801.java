dfe808865367879c7d6a4fb5c36562843c87a0e9
zuiguangyin <2247359268@qq.com>
2020-09-17 01:05:09 +0800

    🎗👕👕修改名字，修改配置，使用feign的方式

--- springcloud-consumer-feign-8801/src/main/java/com/zgy/springcloud/SpringcloudConsumerFeign8801.java
+++ springcloud-consumer-feign-8801/src/main/java/com/zgy/springcloud/SpringcloudConsumerFeign8801.java
@@ -1,16 +1,12 @@
 package com.zgy.springcloud;
 
-import com.zgy.springcloud.config.MyRoundRobinRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
-// 使用自定义的负责均衡规则，name指的是对那个服务进行负载均衡
-@RibbonClient(name = "SPRINGCLOUD-PROVIDER", configuration = MyRoundRobinRule.class)
 public class SpringcloudConsumerFeign8801 {
 
     public static void main(String[] args) {
--- springcloud-consumer-feign-8801/src/main/java/com/zgy/springcloud/controller/PoliceConsumer8801FeignController.java
+++ springcloud-consumer-feign-8801/src/main/java/com/zgy/springcloud/controller/PoliceConsumer8801FeignController.java
@@ -1,6 +1,7 @@
 package com.zgy.springcloud.controller;
 
 import com.zgy.springcloud.pojo.Police;
+import com.zgy.springcloud.service.PoliceClientService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,20 +17,26 @@ import org.springframework.web.client.RestTemplate;
  */
 @Slf4j
 @RestController
-@RequestMapping("consumer8701/police")
-public class PoliceConsumer8701Controller {
+@RequestMapping("consumer8801/police")
+public class PoliceConsumer8801FeignController {
     @Autowired
     private RestTemplate restTemplate;
 
+    // 这是RestTemplate的方式
     // private static final String restURL = "http://127.0.0.1:8001";
     // 上面是传统的方式，下面是通过注册中心的方式，就可以获取我们想要的信息了
 
+    // 这是Ribbon的方式
     // 使用了Ribbon之后, 这个地址应该是一个变量，通过服务名来访问, 下面两种都可以
     // private static final String restURL = "http://springcloud-provider";
-    private static final String restURL = "http://SPRINGCLOUD-PROVIDER";
+    // private static final String restURL = "http://SPRINGCLOUD-PROVIDER";
+    // return restTemplate.getForObject(restURL + "/police/get?id=" + id, Police.class);
+
+    @Autowired
+    PoliceClientService service;
     @GetMapping("get")
     public Police get(Integer id) {
         log.info("准备从远程获取police, id是：{}!", id);
-        return restTemplate.getForObject(restURL + "/police/get?id=" + id, Police.class);
+        return service.getPolice(id);
     }
 }
