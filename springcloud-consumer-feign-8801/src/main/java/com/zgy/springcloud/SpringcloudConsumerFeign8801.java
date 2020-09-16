25d8b4124cb96bf9694de1649d151cf6791e795a
zuiguangyin <2247359268@qq.com>
2020-09-17 01:14:45 +0800

    🏀🏀🏀开启扫描，在配置的包之中，扫描feign的客户端

--- springcloud-consumer-feign-8801/src/main/java/com/zgy/springcloud/SpringcloudConsumerFeign8801.java
+++ springcloud-consumer-feign-8801/src/main/java/com/zgy/springcloud/SpringcloudConsumerFeign8801.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
+@EnableFeignClients(basePackages = {"com.zgy.springcloud"})
 public class SpringcloudConsumerFeign8801 {
 
     public static void main(String[] args) {
