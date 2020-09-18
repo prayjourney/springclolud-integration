174f38da31361e08cfe81a795a98e29d0f412b0f
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-09-18 13:34:01 +0800

    🥮🥮🥮修改类名

--- springcloud-provider-8002/src/test/java/com/zgy/springcloud/SpringcloudProvider8002ApplicationTests.java
+++ springcloud-provider-8002/src/test/java/com/zgy/springcloud/SpringcloudProvider8002ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringcloudProvider8001ApplicationTests {
+class SpringcloudProvider8002ApplicationTests {
 
     @Test
     void contextLoads() {
--- springcloud-provider-hystrix-fusing-8003/src/main/java/com/zgy/springcloud/SpringcloudProviderHystrixFusing8003.java
+++ springcloud-provider-hystrix-fusing-8003/src/main/java/com/zgy/springcloud/SpringcloudProviderHystrixFusing8003.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient // 服务启动后自动注册到eureka注册中心
 @EnableDiscoveryClient // 服务可以被发现
-public class SpringcloudProvider8002 {
+public class SpringcloudProviderHystrixFusing8003 {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudProvider8002.class, args);
+        SpringApplication.run(SpringcloudProviderHystrixFusing8003.class, args);
     }
 
 }
