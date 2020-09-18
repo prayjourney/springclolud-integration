39a64ffe0cecb925c30a0dfed83a8919f201db51
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-09-18 14:43:52 +0800

    🌙🥮🌙启用Hystrix

--- springcloud-provider-hystrix-fusing-8003/src/main/java/com/zgy/springcloud/SpringcloudProviderHystrixFusing8003.java
+++ springcloud-provider-hystrix-fusing-8003/src/main/java/com/zgy/springcloud/SpringcloudProviderHystrixFusing8003.java
@@ -2,12 +2,16 @@ package com.zgy.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient // 服务启动后自动注册到eureka注册中心
 @EnableDiscoveryClient // 服务可以被发现
+@EnableHystrix // 启用Hystrix
+// @EnableCircuitBreaker // 启用熔断器，这个注解作用在了@EnableHystrix之上
 public class SpringcloudProviderHystrixFusing8003 {
 
     public static void main(String[] args) {
