6f5b61335879f7cbe8fdba3b26aa92d8153557f7
zuiguangyin <2247359268@qq.com>
2020-09-19 16:28:51 +0800

    update

--- springcloud-api-feign-hystrix/src/main/java/com/zgy/springcloud/service/PoliceClientService.java
+++ springcloud-api-feign-hystrix/src/main/java/com/zgy/springcloud/service/PoliceClientService.java
@@ -16,10 +16,10 @@ import java.util.List;
  * @Description 该接口对应于应用id为SPRINGCLOUD-PROVIDER的微服务
  */
 /**
- * 这个是一个关键，我们使用feign的服务，去调用provider服务提供者的服务，这时候，我们就去会从@FeignClient注解的服务之中，找相同声明的方法，然后实际调用
+ * 这个是一个关键，我们使用feign的服务，去调用provider服务提供者的服务，这时候，我们就去会从@FeignClient注解的服务之中，
+ * 找相同声明的方法，然后实际调用，该接口对应于应用id为SPRINGCLOUD-PROVIDER的微服务，该接口添加到添加到spring管理
  */
-// 该接口对应于应用id为SPRINGCLOUD-PROVIDER的微服务
-@Component // 👕👕👕feign的接口，添加到spring管理
+@Component
 @FeignClient(value = "SPRINGCLOUD-PROVIDER")
 public interface PoliceClientService {
     @PostMapping(value = "/police/add")
