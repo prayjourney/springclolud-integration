f40df96ab894d18c6dfd078e14cd6a50844554af
zuiguangyin <2247359268@qq.com>
2020-09-21 23:30:55 +0800

    ✈✈✈修改类名

--- springcloud-consumer-feign-hystrix-8901/src/main/java/com/zgy/springcloud/SpringcloudConsumerFeign8901.java
+++ springcloud-consumer-feign-hystrix-8901/src/main/java/com/zgy/springcloud/SpringcloudConsumerFeign8901.java
@@ -5,15 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
 @EnableFeignClients(basePackages = {"com.zgy.springcloud"})
-public class SpringcloudConsumerFeign8801 {
+public class SpringcloudConsumerFeign8901 {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudConsumerFeign8801.class, args);
+        SpringApplication.run(SpringcloudConsumerFeign8901.class, args);
     }
 
 }
