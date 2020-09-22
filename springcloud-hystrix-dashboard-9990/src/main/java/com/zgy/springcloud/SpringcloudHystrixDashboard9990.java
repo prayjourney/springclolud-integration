8992f63f540f218886c922d6611c9c86b5a96d01
zuiguangyin <2247359268@qq.com>
2020-09-23 01:52:51 +0800

    🦪🦪🦪开启dashboard,访问http://localhost:9990/hystrix

--- springcloud-hystrix-dashboard-9990/src/main/java/com/zgy/springcloud/SpringcloudHystrixDashboard9990.java
+++ springcloud-hystrix-dashboard-9990/src/main/java/com/zgy/springcloud/SpringcloudHystrixDashboard9990.java
@@ -3,9 +3,7 @@ package com.zgy.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableHystrixDashboard // 开启dashboard
