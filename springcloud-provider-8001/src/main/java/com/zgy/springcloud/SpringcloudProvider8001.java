87887d76e064295e7b927847fd99173c6070e64b
zuiguangyin <2247359268@qq.com>
2020-09-25 18:58:01 +0800

    🙉🙉🙉添加暴露监控信息配置

--- springcloud-provider-8001/src/main/java/com/zgy/springcloud/SpringcloudProvider8001.java
+++ springcloud-provider-8001/src/main/java/com/zgy/springcloud/SpringcloudProvider8001.java
@@ -1,9 +1,12 @@
 package com.zgy.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient // 服务启动后自动注册到eureka注册中心
@@ -12,6 +15,16 @@ public class SpringcloudProvider8001 {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudProvider8001.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 }
