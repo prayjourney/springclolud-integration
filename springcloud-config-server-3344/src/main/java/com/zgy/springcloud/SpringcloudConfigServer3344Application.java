eb89b501c3467f39feee31439d6fe22f26f08906
zuiguangyin <2247359268@qq.com>
2020-09-27 01:47:06 +0800

    🍜🍜🍜将服务注册到eureka中心

--- springcloud-config-server-3344/src/main/java/com/zgy/springcloud/SpringcloudConfigServer3344Application.java
+++ springcloud-config-server-3344/src/main/java/com/zgy/springcloud/SpringcloudConfigServer3344Application.java
@@ -2,10 +2,14 @@ package com.zgy.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer // 开启ConfigServer的服务
+@EnableEurekaClient // 服务启动后自动注册到eureka注册中心
+@EnableDiscoveryClient // 服务可以被发现
 public class SpringcloudConfigServer3344Application {
 
     public static void main(String[] args) {
