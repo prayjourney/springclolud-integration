bd279fd0b41164fc953dcbcbf60dc5c55208bc48
zuiguangyin <2247359268@qq.com>
2020-09-26 23:17:54 +0800

    🍳🍳🍳开启Zuul代理

--- springcloud-zuul-8899/src/main/java/com/zgy/springcloud/SpringcloudZuul8899Application.java
+++ springcloud-zuul-8899/src/main/java/com/zgy/springcloud/SpringcloudZuul8899Application.java
@@ -2,8 +2,10 @@ package com.zgy.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy // 开启Zuul代理
 public class SpringcloudZuul8899Application {
 
     public static void main(String[] args) {
