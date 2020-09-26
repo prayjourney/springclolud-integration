378df2ee6184d1d760e0917ac6624152152d3c53
zuiguangyin <2247359268@qq.com>
2020-09-27 01:31:11 +0800

    🍗🍗🍗config-server的初始化

---
+++ springcloud-config-server-3344/src/main/java/com/zgy/springcloud/SpringcloudConfigServer3344Application.java
@@ -0,0 +1,13 @@
+package com.zgy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudConfigServer3344Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConfigServer3344Application.class, args);
+    }
+
+}
---
+++ springcloud-config-server-3344/src/test/java/com/zgy/springcloud/SpringcloudConfigServer3344ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgy.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudConfigServer3344ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
