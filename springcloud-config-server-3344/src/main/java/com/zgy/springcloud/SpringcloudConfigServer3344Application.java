3dcf0f4756c6d1db72237d51b89190776034c5d9
zuiguangyin <2247359268@qq.com>
2020-09-27 01:38:35 +0800

    🍗🍗🍗开启ConfigServer的服务

--- springcloud-config-server-3344/src/main/java/com/zgy/springcloud/SpringcloudConfigServer3344Application.java
+++ springcloud-config-server-3344/src/main/java/com/zgy/springcloud/SpringcloudConfigServer3344Application.java
@@ -2,8 +2,10 @@ package com.zgy.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer // 开启ConfigServer的服务
 public class SpringcloudConfigServer3344Application {
 
     public static void main(String[] args) {
