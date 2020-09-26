f790bcf930d569b4a5427d802dbed149b28f45ce
zuiguangyin <2247359268@qq.com>
2020-09-26 22:52:32 +0800

    🍟🍟🍟zuul模块初始化

---
+++ springcloud-zuul-8899/src/main/java/com/zgy/springcloud/SpringcloudZuul8899Application.java
@@ -0,0 +1,13 @@
+package com.zgy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudZuul8899Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudZuul8899Application.class, args);
+    }
+
+}
