91d94fc00c2d008883c226db1618ea7f72626810
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-09-27 14:31:27 +0800

    🍼🍬🍭用于测试的config的client，只是获取相关的配置，没有太多的实际作用，项目的初始化

---
+++ springcloud-config-client-3355/src/main/java/com/zgy/springcloud/SpringcloudConfigClient3355.java
@@ -0,0 +1,13 @@
+package com.zgy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudConfigClient3355 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConfigClient3355.class, args);
+    }
+
+}
